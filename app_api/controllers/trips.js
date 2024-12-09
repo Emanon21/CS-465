15c76f1522d39bc2cad30e6b89dd474f1141d479
Angelo Mangalindan <gelom.bear@gmail.com>
2024-12-08 17:43:04 -0800

    Module 6 Completed Baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -28,6 +28,7 @@ const tripsList = async(req, res) => {
     }
 };
 
+
 // GET: /trips/:tripCode - lists a single trip
 // Regardless of outcome, response must include HTML status code
 // and JSON message to the requestion client
@@ -54,7 +55,68 @@ const tripsFindByCode = async(req, res) => {
     }
 };
 
+// PUT: /trips/:tripCode - Updates a Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsUpdateTrip = async (req, res) => {
+  console.log(req.params);
+  console.log(req.body);
+  try {
+    const q = await Model.findOneAndUpdate(
+      { code: req.params.tripCode },
+      {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description,
+      }
+    ).exec();
+
+    if (!q) {
+      // Database returned no data
+      return res.status(404).json({ message: "Trip not found" });
+    } else {
+      // Return resulting updated trip
+      return res.status(200).json(q);
+    }
+
+    // Uncomment the following line to show results of operation
+    // console.log(q);
+  } catch (error) {
+    console.error("Error updating trip:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const tripsAddTrip = async (req, res) => {
+    const newTrip = new Trip({
+      code: req.body.code,
+      name: req.body.name,
+      length: req.body.length,
+      start: req.body.start,
+      resort: req.body.resort,
+      perPerson: req.body.perPerson,
+      image: req.body.image,
+      description: req.body.description,
+    });
+  
+    const q = await newTrip.save();
+  
+    if (!q) {
+      //Database returned no data
+      return res.status(400).json(err);
+    } else {
+      return res.status(201).json(q);
+    }
+  };
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,8 +5,8 @@ const router = express.Router(); // Router logic
 const tripsController = require('../controllers/trips');
 
 // define route for our trips endpoint
-router.route("/trips").get(tripsController.tripsList);
+router.route("/trips").get(tripsController.tripsList).post(tripsController.tripsAddTrip);
 
-router.route('/trips/:tripCode').get(tripsController.tripsFindByCode);
+router.route('/trips/:tripCode').get(tripsController.tripsFindByCode).put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
